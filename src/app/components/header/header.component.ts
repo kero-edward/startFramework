22f675149783d79ba300c4d3a669fd6505dfdabf
kero-edward <kyrillos.edward17@gmail.com>
2024-02-07 03:53:55 +0200

    finished from home component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  isScrolling: boolean = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    console.log('scrollY: ', scrollY);
+    if (scrollY >= 50) {
+      this.isScrolling = true;
+    } else {
+      this.isScrolling = false;
+    }
+  }
 }
